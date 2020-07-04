277c600941537bcfcfce73d06c2e9fc7596eed75
mikechen66 <875661606@qq.com>
2020-07-04 23:29:41 +0800

    alexnet.py
    It is the object-oriented AlexNet model in TensorFlow 2.x.  It addresses the whole structure of the original model in 2012. 
    To be called by the application of alexnet_classifying.py, the statement of number_class = 2 is commenced (deleted). 
    
    The User Guide: 
    
    Part One
    To use the script in Python, users need to create the folder such as AlexNet-tf2. The application 
    automatically downloads the pictures into the created folders. 
    
    Part Two. Script running procedure
    
    1. Enter into current directory
    
       $ cd /home/mike/Documents/AlexNet-tf2
    
    Anaconda defaults the pre-installed Python3 and the Ubuntu 18.04 has both Python2 and Python3. Therefore, 
    users need to follow the procedures. 
    
    1. Script running command
    
      In the Conda Environment, please execute the following command in the Ubuntu terminal at the current 
      directory.  
      
      $ python alexnet_classifying.py 
      
      or 
    
      In the native Ubuntu 18.04 env, please execute the following command. 
    
      $ python3 alexnet_classifying.py 
    
      While executing the above-mentioned command, the Linux Terminal shows the arrays of image name ended 
      with jpg. 
    
      Moreover, the Terminal show the complete Model: alex_net". Furthermore, it show "Found 117 images 
      belonging to 2 classes". 
    
      In the meantime, it also address the following warning. However, users can ignore the warning becuase it
      does not influence the script running. 
    
      WARNING:tensorflow:sample_weight modes were coerced from
      ...
        to  
      ['...']
      
    
    2. Start the TensorBoard
    
       After completing the script excution, users can start the TensorBoard command in the Linux Terminal 
       at the current directory. 
    
      $ tensorboard --logdir logs/fit
    
      After the above-mentioned command is given, the ｔerminal shows the reminding message as follows. 
      Serving TensorBoard on localhost; to expose to the network, use a proxy or pass --bind_all
      TensorBoard 2.2.1 at http://localhost:6006/ (Press CTRL+C to quit)
    
    3. Enter the weblink in a browser
       After entering the weblink into either Chrome or Firefox browser, the TensorBoard will show the diagrams
       that the scrip defines. 
       http://localhost:6006/
    
    4. Images showing 
       The browser could not show the images. If users want to plot the images, please upload the Python script 
       into the Jupyter Notebook or just directly adopts the original ipython script.

---
+++ alexnet.py
@@ -0,0 +1,94 @@
+# AlexNet Model 
+
+"""
+AlexNet, Krizhevsky, Alex, Ilya Sutskever and Geoffrey E. Hinton, 2012
+https://papers.nips.cc/paper/4824-imagenet-classification-with-deep-convolutional-neural-networks.pdf
+
+Michael Guerzhoy and Davi Frossard, 2016
+AlexNet implementation in TensorFlow, with weights Details: 
+#http://www.cs.toronto.edu/~guerzhoy/tf_alexnet/
+
+The script is the realizatio of object oriented style of AlexNet. The construction method of AlexNet 
+includes three parameters, including self, input_shape, num_classes, of which, input_shape works as a 
+placeholder. 
+"""
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+
+# -img_rows, img_cols = 227, 227
+# -input_shape = (img_rows, img_cols, 3)
+# -num_classes = 2
+
+# Define the AlexNet Model 
+class AlexNet(Sequential):
+    def __init__(self, input_shape, num_classes):
+        super().__init__()
+
+        # No.1 Convolutional Layer: (227- 11 + 2 * 0) / 4 + 1 = 55
+        self.add(Conv2D(filters=96, kernel_size=(11,11), strides=4,
+                        padding='valid', activation='relu',
+                        input_shape=input_shape, kernel_initializer='GlorotNormal'))
+        # Max Pooling: (55- 3 + 2 * 0) / 2 + 1 = 27
+        self.add(MaxPooling2D(pool_size=(3,3), strides=(2,2),
+                              padding='valid', data_format=None))
+        
+        # No.2 Conv Layer: (27- 5 + 2 * 2) / 1 + 1 = 27
+        self.add(Conv2D(filters=256, kernel_size=(5,5), strides=1,
+                        padding='same', activation='relu',
+                        kernel_initializer='GlorotNormal'))
+        # Max Pooling: (27-  3 + 2 * 0) / 2 + 1 = 13
+        self.add(MaxPooling2D(pool_size=(3,3), strides=(2,2),
+                              padding='valid', data_format=None))
+        
+        # No.3 Conv Layer: (13 - 3 + 2 * 1) / 1 + 1 = 13
+        self.add(Conv2D(filters=384, kernel_size=(3,3), strides=1,
+                        padding='same', activation='relu',
+                        kernel_initializer='GlorotNormal'))
+
+        # No.4 Conv Layer: (13 - 3 + 2 * 1) / 1 + 1 = 13
+        self.add(Conv2D(filters=384, kernel_size=(3,3), strides=1,
+                 padding='same', activation='relu',
+                 kernel_initializer='GlorotNormal'))
+
+        # No.5 Conv Layer: (13 - 3 + 2 * 1) / 1 + 1 = 13
+        self.add(Conv2D(filters=256, kernel_size=(3,3), strides=1,
+                 padding='same', activation='relu',
+                 kernel_initializer='GlorotNormal')) 
+        # Max Pooling: (13 - 3 + 2 * 0) / 2 + 1 =  6
+        self.add(MaxPooling2D(pool_size=(3,3), strides=(2,2),
+                              padding='valid', data_format=None))
+
+        # # Flatten the three dimensions of 256 x 6 x 6 into one dimension of 9216.
+        self.add(Flatten())
+
+        # No.6 FC Layer 
+        self.add(Dense(4096, activation='relu'))
+        # Add Dropout
+        self.add(Dropout(0.5))
+        
+        # No.7 FC Layer         
+        self.add(Dense(4096, activation='relu'))
+        # Add Dropout 
+        self.add(Dropout(0.5))
+
+        # No.8 FC Layer
+        self.add(Dense(1000, activation='relu'))
+        self.add(Dense(num_classes, activation='softmax'))
+
+        # Compile the model
+        self.compile(optimizer=tf.keras.optimizers.Adam(0.001),
+                     loss='categorical_crossentropy',
+                     metrics=['accuracy'])
+
+# Call the AlexNet model 
+# -model = AlexNet((227, 227, 3), num_classes)
+
+# show the full model structure of AlexNet 
+# -model.summary()
+
+
+
+
+        
