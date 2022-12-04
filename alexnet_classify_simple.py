17041cdfbb4db35b1bef3f68c5df397d4a1b1f4c
mikechen66 <875661606@qq.com>
2022-12-04 21:56:52 +0800

    Add files via upload

--- alexnet_classify_simple.py
+++ alexnet_classify_simple.py
@@ -1,14 +1,90 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-"""
-After completion of downloading the data with alexnet_classify.py, users can use the lightweight
-script to call alexnet.py. Therefore, the download and processing lines of code are deleted. To 
-run the script on the environment of TensorFlow 2.2.0 and CUDA 11.0/cudNN 8.0.1, users need to 
-add -cap-add=CAP_SYS_ADMIN
+# It is originally written with ipython by datahacker and re-written with python and adding some 
+# lines of code in order to adapt the Python practice by Mike Chen. 
+# http://media5.datahacker.rs/2018/06/logo-crno.png)
 
-$ python3 alexnet_classify_simple.py --cap-add=CAP_SYS_ADMIN
 """
+The User Guide: 
+
+Part One
+
+To use the script in Python, users need to create the folder such as AlexNet_Callback. The application 
+automatically downloads the pictures into the created folders. 
+
+Part Two. Script running procedure
+
+1. Enter into current directory
+
+   $ cd /home/john/Documents/Alexnet_Callback
+
+Anaconda defaults the pre-installed Python3 and the Ubuntu 18.04 has both Python2 and Python3. Therefore, 
+users need to follow the procedures. 
+
+2. Script running command
+
+  In the Conda Environment, please execute the following command in the Ubuntu terminal at the current 
+  directory.  
+  
+  $ python alexnet_classify.py  
+  
+  or 
+
+  In the native Ubuntu 18.04 env, please execute the following command. 
+
+  $ python3 alexnet_classify.py
+
+  While executing the above-mentioned command, the Linux Terminal shows the arrays of image name ended 
+  with jpg. 
+
+  Moreover, the Terminal show the complete Model: alex_net". Furthermore, it show "Found 117 images 
+  belonging to 2 classes". 
+
+  In the meantime, it also address the following warning. However, users can ignore the warning becuase it
+  does not influence the script running. 
+
+  WARNING:tensorflow:sample_weight modes were coerced from
+  ...
+    to  
+  ['...']
+  
+
+3. Start the TensorBoard
+
+   After completing the script excution, users can start the TensorBoard command in the Linux Terminal 
+   at the current directory. 
+
+  $ tensorboard --logdir logs/fit
+
+  After the above-mentioned command is given, the ｔerminal shows the reminding message as follows. 
+  Serving TensorBoard on localhost; to expose to the network, use a proxy or pass --bind_all
+  TensorBoard 2.2.1 at http://localhost:6006/ (Press CTRL+C to quit)
+
+4. Enter the weblink in a browser
+
+   http://localhost:6006/
+
+   After entering the weblink into either Chrome or Firefox browser, the TensorBoard will show the diagrams
+   that the scrip defines. 
+
+5. Images showing 
+
+   The browser could not show the images. If users want to plot the images, please upload the Python script 
+   into the Jupyter Notebook or just directly adopts the original ipython script. 
+
+Part Three Trouble shooting 
+
+Issue: 
+AttributeError: module 'tensorflow' has no attribute 'compat'
+
+Solution: 
+It is the conflict between Conda and TensorFlow 2.x if users adopt the Anaconda/Miniconda env. I recommend 
+the users to install tensorflow 2.1 and then install tensorflw-estimator as follows. 
+
+$ conda install tensorflow-estimator==2.1.0
+"""
+
 
 import datetime
 import numpy as np
@@ -32,48 +108,108 @@ import numpy as np
 from bs4 import BeautifulSoup
 
 
-# Set up the GPU in the condition of allocation exceeds system memory.The following lines 
-# of code can avoids the sudden stop of the runtime. 
+"""
+# Get the ship synset
+page = requests.get("http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=n04194289")
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# Get the bicycle synset
+bikes_page = requests.get("http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=n02834778")
+bikes_soup = BeautifulSoup(bikes_page.content, 'html.parser')
+
+str_soup=str(soup)
+split_urls=str_soup.split('\r\n')
+
+bikes_str_soup=str(bikes_soup)
+bikes_split_urls=bikes_str_soup.split('\r\n')
+"""
+
+
+# Confirm the directories 
+path1 = '/home/mic/Documents/Alexnet_Callback/content/train'
+path2 = '/home/mic/Documents/Alexnet_Callback/content/train/ships'
+path3 = '/home/mic/Documents/Alexnet_Callback/content/train/bikes'
+path4 = '/home/mic/Documents/Alexnet_Callback/content/validation'
+path5 = '/home/mic/Documents/Alexnet_Callback/content/validation/ships'
+path6 = '/home/mic/Documents/Alexnet_Callback/content/validation/bikes'
+
+
+# Change the dimentions of input pixel images to 32 x 32 
+img_rows, img_cols = 32, 32
+input_shape = (img_rows, img_cols, 3)
+
+"""
+def url_to_image(url):
+    resp = urllib.request.urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+    return image
+
+num_of_training_images=100
+for progress in range(num_of_training_images):
+    if not split_urls[progress] == None:
+        try:
+            img = url_to_image(split_urls[progress])
+            if (len(img.shape))==3:
+                save_path = '/home/mike/Documents/Alexnet_Callback/content/train/ships/img'+str(progress)+'.jpg'
+                cv2.imwrite(save_path,img)
+        except:
+            None
+
+for progress in range(num_of_training_images):
+    if not bikes_split_urls[progress] == None:
+        try:
+            img = url_to_image(bikes_split_urls[progress])
+            if (len(img.shape))==3:
+                save_path = '/home/mike/Documents/Alexnet_Callback/content/train/bikes/img'+str(progress)+'.jpg'
+                cv2.imwrite(save_path,img)
+        except:
+            None
+
+num_of_validation_images=50
+for progress in range(num_of_validation_images):
+    if not split_urls[progress] == None:
+        try:
+            img = url_to_image(split_urls[num_of_training_images+progress])
+            if (len(img.shape))==3:
+                save_path = '/home/mike/Documents/Alexnet_Callback/content/validation/ships/img'+str(progress)+'.jpg'
+                cv2.imwrite(save_path,img)
+        except:
+            None
+
+for progress in range(num_of_validation_images):
+    if not bikes_split_urls[progress] == None:
+        try:
+            img = url_to_image(bikes_split_urls[num_of_training_images+progress])
+            if (len(img.shape))==3:
+                save_path = '/home/mike/Documents/Alexnet_Callback/content/validation/bikes/img'+str(progress)+'.jpg'
+                cv2.imwrite(save_path,img)
+        except:
+            None
+"""
+ 
+
+# Modify the original ipython code into the lines of code in Python          
+array1 = os.listdir('/home/mic/Documents/Alexnet_Callback/content/train/ships')
+array2 = os.listdir('/home/mic/Documents/Alexnet_Callback/content/train/bikes')
+array3 = os.listdir('/home/mic/Documents/Alexnet_Callback/content/validation/ships')
+array4 = os.listdir('/home/mic/Documents/Alexnet_Callback/content/validation/bikes')
+# Add the print function to show the results of images ended with jpg
+print(array1, array2, array3, array4) 
+
+
+# Set up the GPU in the condition of allocation exceeds system memory with the reminding message: Could not 
+# create cuDNN handle... The following lines of code can avoids the sudden stop of the runtime. 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
 
-# Confirm the directories 
-path1 = '/home/mike/Documents/Alexnet_Callback/content/train'
-path2 = '/home/mike/Documents/Alexnet_Callback/content/train/ships'
-path3 = '/home/mike/Documents/Alexnet_Callback/content/train/bikes'
-path4 = '/home/mike/Documents/Alexnet_Callback/content/validation'
-path5 = '/home/mike/Documents/Alexnet_Callback/content/validation/ships'
-path6 = '/home/mike/Documents/Alexnet_Callback/content/validation/bikes'
-
-
 # It has pre-defined two classes including bike and ship. 
-EPOCHS = 100
-BATCH_SIZE = 32
-image_width = 227
-image_height = 227
-channels = 3
 num_classes = 2
 
-
-# Change the original images dimentions to 32 x 32. 
-img_rows, img_cols = 32, 32
-input_shape = (img_rows, img_cols, 3)
-
- 
-# Modify the original ipython code into the lines of code in Python          
-array1 = os.listdir('/home/mike/Documents/Alexnet_Callback/content/train/ships')
-array2 = os.listdir('/home/mike/Documents/Alexnet_Callback/content/train/bikes')
-array3 = os.listdir('/home/mike/Documents/Alexnet_Callback/content/validation/ships')
-array4 = os.listdir('/home/mike/Documents/Alexnet_Callback/content/validation/bikes')
-
-
-# Add the print function to show the results of images ended with jpg
-print(array1, array2, array3, array4) 
-
-
-# Call the alexnet model in alexnet.py
+# It calls the alexnet model in alexnet.py
 model = AlexNet((227, 227, 3), num_classes)
 
 # Compile the model
@@ -85,13 +221,17 @@ model.compile(optimizer=tf.keras.optimizers.Adam(0.001),
 model.summary()
 
 
-# Designate the directories for training, validation and model saved. 
-train_dir = '/home/mike/Documents/Alexnet_Callback/content/train'
-valid_dir = '/home/mike/Documents/Alexnet_Callback/content/validation'
-model_dir = '/home/mike/Documents/Alexnet_Callback/content/my_model.h5'
+# Give some training parameters
+EPOCHS = 100
+BATCH_SIZE = 32
+image_height = 227
+image_width = 227
+train_dir = '/home/mic/Documents/Alexnet_Callback/content/train'
+valid_dir = '/home/mic/Documents/Alexnet_Callback/content/validation'
+model_dir = '/home/mic/Documents/Alexnet_Callback/content/my_model.h5'
 
 
-# Assign both the image and the diretory generators
+# Generate batches of tensor image data with real-time data augmentation.
 train_datagen = ImageDataGenerator(rescale=1./255,
                                    rotation_range=10,
                                    width_shift_range=0.1,
@@ -99,6 +239,7 @@ train_datagen = ImageDataGenerator(rescale=1./255,
                                    shear_range=0.1,
                                    zoom_range=0.1)
 
+# Takes the path to a directory & generates batches of augmented data.
 train_generator = train_datagen.flow_from_directory(train_dir,
                                                     target_size=(image_height, image_width),
                                                     color_mode="rgb",
@@ -108,6 +249,7 @@ train_generator = train_datagen.flow_from_directory(train_dir,
                                                     class_mode="categorical")
 
 valid_datagen = ImageDataGenerator(rescale=1.0/255.0)
+
 valid_generator = valid_datagen.flow_from_directory(valid_dir,
                                                     target_size=(image_height, image_width),
                                                     color_mode="rgb",
@@ -121,14 +263,16 @@ train_num = train_generator.samples
 valid_num = valid_generator.samples
 
 
-# Need to start the following command in Ubuntu Terminal after executing the script. 
-# tensorboard --logdir logs/fit
+# Start Tensorboard --logdir logs/fit
 log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
 callback_list = [tensorboard_callback]
 
+# Show the diagram once upon entering the weblink in a browser
+# http://localhost:6006/
 
-# Set up the verbose=1 (or verbose=1) for visible (or invisible) epochs. 
+# The system starts the training. We change the verbose=0 to verbose=1. So users can see the dynamic 
+# precudure of training and validation execution. 
 model.fit(train_generator,
           epochs=EPOCHS,
           steps_per_epoch=train_num//BATCH_SIZE,
@@ -136,7 +280,6 @@ model.fit(train_generator,
           validation_steps=valid_num//BATCH_SIZE,
           callbacks=callback_list,
           verbose=1)
-
 
 # The system saves the whole model into the direcotry: /home/mike/Documents/AlexNet-tf2/content. The 
 # model of my_model.h5 has the quite big size of 748.6 MB. 
@@ -148,7 +291,6 @@ model.save(model_dir)
 class_names = ['bike', 'ship']
 x_valid, label_batch  = next(iter(valid_generator))
 prediction_values = model.predict_classes(x_valid)
-
 
 # The plot will be realized in the Jupyter Notebook after running the script in either Python or 
 # ipython. 
